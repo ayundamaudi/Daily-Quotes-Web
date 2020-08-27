9fb5835c0c1037e97061e7ee9c1240bdc404c0d2
ayundamaudi <maudiatamaayunda@gmail.com>
2020-08-28 05:25:42 +0700

    delete link script

--- js/javascript.js
+++ js/javascript.js
@@ -6,8 +6,8 @@ fetch('https://programming-quotes-api.herokuapp.com/quotes/lang/en')
     .then(response => response.json())
     .then(result => {
         myArray = result;
+        console.log(myArray)
         setToDOM();
-        console.log(myArray);
     })
 
 // <!-- function to change each slide -->
@@ -28,13 +28,15 @@ function setToDOM() {
     var pict = ["/img/image-1.png", "/img/image-2.png", "/img/image-3.png"];
     // array of color quote
     var color_quoted = ["2f80ed","eb5757","219653","aqua", "mediumvioletred", "brown","darkorange", "deeppink", "maroon", "purple"];
-
+// console.log(url('${pict[slideIndex % (pict.length)]}'));
     // description quote
     document.getElementById(`desc-content`).innerHTML = myArray[slideIndex].en;
     // author name of quote
     document.getElementById(`name-author`).innerHTML = myArray[slideIndex].author;
     // picture of each slide
     document.getElementById(`mySlides`).style.background = `url('${pict[slideIndex % (pict.length)]}')`;
+    
     // color of each slide
     document.getElementById(`blockquote-quoted`).style.color = color_quoted[slideIndex % (color_quoted.length)];
 }
+
