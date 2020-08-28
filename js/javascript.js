55a6c9270a3533f0aefd4d00df2f9deaf4896661
ayundamaudi <maudiatamaayunda@gmail.com>
2020-08-28 15:47:33 +0700

    tidying function

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,7 @@ fetch('https://programming-quotes-api.herokuapp.com/quotes/lang/en')
         setToDOM();
     })
 
-// <!-- function to change each slide -->
+// <!-- function next slide  -->
 function plusSlides(n) {
     slideIndex += n;
     if (slideIndex == myArray.length) {
@@ -19,6 +19,7 @@ function plusSlides(n) {
     setToDOM();
 }
 
+// <!-- function prev slide -->
 function prevSlides(n){
     slideIndex += n;
     if (slideIndex < 0) {
@@ -27,20 +28,23 @@ function prevSlides(n){
     setToDOM();
 }
 
+// <!-- function random color quote -->
 function random_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var random_bg_imgColor = "rgb(" + x + "," + y + "," + z + ")";
+    var a = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var c = Math.floor(Math.random() * 255);
+    var color_quote = "rgb(" + a + "," + b + "," + c + ")";
 
- document.getElementById(`blockquote-quoted`).style.color = random_bg_imgColor;
+    document.getElementById(`blockquote-quoted`).style.color = color_quote;
 }
 
+// <!-- function random bg img -->
 function random_bg_img() {
-    var dir = '/img';
-    var images = ["/image1.jpg", "/image2.jpg", "/image3.jpg","/image4.jpg","/image5.jpg","/image6.jpg","/image7.jpg","/image8.jpg","/image9.jpg","/image10.jpg","/image11.jpg"];
-    var randomCount = (Math.floor(Math.random() * images.length));
-    document.getElementById(`mySlides`).setAttribute("style", "background-image: url(" + dir + images[randomCount]);
+    var folder = '/img';
+    var imagesArray = ["/image1.jpg", "/image2.jpg", "/image3.jpg","/image4.jpg","/image5.jpg","/image6.jpg","/image7.jpg","/image8.jpg","/image9.jpg","/image10.jpg","/image11.jpg"];
+    var bg_img = (Math.floor(Math.random() * imagesArray.length));
+    
+    document.getElementById(`mySlides`).setAttribute("style", "background-image: url(" + folder + imagesArray[bg_img]);
 }
 
 // <!-- function data to DOM --> 
@@ -49,7 +53,6 @@ function setToDOM() {
     document.getElementById(`desc-content`).innerHTML = myArray[slideIndex].en;
     // author name of quote
     document.getElementById(`name-author`).innerHTML = myArray[slideIndex].author;
-
-    random_color();
-    random_bg_img();
+    random_color(); // random color quote
+    random_bg_img(); // random color bg img
 }
