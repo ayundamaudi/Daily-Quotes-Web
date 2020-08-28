81edc89bcbe9e75484b3d0b7c1700ea36e654e8d
ayundamaudi <maudiatamaayunda@gmail.com>
2020-08-28 07:10:59 +0700

    adding image and color in array

--- js/javascript.js
+++ js/javascript.js
@@ -17,7 +17,7 @@ function plusSlides(n) {
         slideIndex = 0;
     }
     else if (slideIndex < 0) {
-        slideIndex = myArray.length - 1;
+        slideIndex = myArray.length-1;
     }
     setToDOM();
 }
@@ -25,9 +25,9 @@ function plusSlides(n) {
 // <!-- function data to DOM --> 
 function setToDOM() {
     // array of picture
-    var pict = ["/img/image-1.png", "/img/image-2.png", "/img/image-3.png"];
+    var pict = ["/img/image-1.png", "/img/image-2.png", "/img/image-3.png","/img/image4.jpg","/img/image5.jpg","/img/image6.jpg","/img/image7.jpg","/img/image8.jpg","/img/image9.jpg"];
     // array of color quote
-    var color_quoted = ["2f80ed","eb5757","219653"];
+    var color_quoted = ["2f80ed","eb5757","219653","aqua","purple","yellow","brown","darkorange","hotpink"];
 // console.log(url('${pict[slideIndex % (pict.length)]}'));
     // description quote
     document.getElementById(`desc-content`).innerHTML = myArray[slideIndex].en;
@@ -35,7 +35,6 @@ function setToDOM() {
     document.getElementById(`name-author`).innerHTML = myArray[slideIndex].author;
     // picture of each slide
     document.getElementById(`mySlides`).style.background = `url('${pict[slideIndex % (pict.length)]}')`;
-    
     // color of each slide
     document.getElementById(`blockquote-quoted`).style.color = color_quoted[slideIndex % (color_quoted.length)];
 }
