62cf2576a15310e9d9e3d7b6be8c9686bb7b5331
ayundamaudi <maudiatamaayunda@gmail.com>
2020-08-29 10:18:42 +0700

    add 24 bg image and change the code of showing the image

--- js/javascript.js
+++ js/javascript.js
@@ -40,11 +40,16 @@ function random_color() {
 
 // <!-- function random bg img -->
 function random_bg_img() {
-    var folder = '/img';
-    var imagesArray = ["/image1.jpg", "/image2.jpg", "/image3.jpg","/image4.jpg","/image5.jpg","/image6.jpg","/image7.jpg","/image8.jpg","/image9.jpg","/image10.jpg","/image11.jpg"];
+    var folder = '/img/';
+    var imagesArray = ["image1.jpg", "image2.jpg", "image3.jpg","image4.jpg","image5.jpg","image6.jpg","image7.jpg","image8.jpg","image9.jpg","image10.jpg","image11.jpg", ,"image12.jpg","image13.jpg","image14.jpg","image15.jpg","image16.jpg","image17.jpg","image18.jpg","image19.jpg","image20.jpg","image21.jpg","image22.jpg","image23.jpg","image24.jpg","image25.jpg","image26.jpg","image27.jpg","image28.jpg","image29.jpg","image30.jpg","image31.jpg","image32.jpg","image33.jpg","image34.jpg","image35.jpg"];
     var bg_img = (Math.floor(Math.random() * imagesArray.length));
+
+    if (imagesArray[bg_img]===undefined) {
+        bg_img = (Math.floor(Math.random() * imagesArray.length));
+    }
     
-    document.getElementById(`mySlides`).setAttribute("style", "background-image: url(" + folder + imagesArray[bg_img]);
+    // document.getElementById(`mySlides`).setAttribute("style", "background-image: url(" + folder + imagesArray[bg_img]);
+    document.getElementById(`mySlides`).style.background = `url('${folder+imagesArray[bg_img]}')`;
 }
 
 // <!-- function data to DOM --> 
